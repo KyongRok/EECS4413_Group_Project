7c185f2cb0a76a2eec344525d93a7799aaf35d36
KyongRok <kyongrokk@gmail.com>
2023-07-24 20:53:05 -0400

    First Commit front end

---
+++ src/main/webapp/webcomponent.js
@@ -0,0 +1,38 @@
+//start of login icon web-component
+class LoginIcon extends HTMLElement{
+connectedCallback(){
+		this.innerHTML = `
+		
+		<a href="login.html">
+			<image src ="images/icons8-login-64.png">
+		</a><br>
+			Login
+		`;
+	}
+}
+customElements.define("login-icon" ,LoginIcon);
+//end of login icon web-component
+
+//start of login button web-component
+class LoginButton extends HTMLElement{
+	connectedCallback(){
+		//edit action
+		this.innerHTML = `
+			<input type="submit" value ='login'>
+		
+		`;
+	}
+}
+customElements.define("login-button",LoginButton);
+//end of login button web-component
+
+//start of register icon web-component
+class RegisterButton extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `
+		<input type="submit" value ='register'>
+		`;
+	}
+}
+customElements.define("register-button" , RegisterButton);
+//end of register web-component
