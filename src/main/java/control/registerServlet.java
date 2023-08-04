e5377dc0630201f8cf231d9e504baa8d11090ce5
Rosabbagh <140211804+Rosabbagh@users.noreply.github.com>
2023-08-04 01:09:34 -0400

    Update registerServlet.java

--- src/main/java/control/registerServlet.java
+++ src/main/java/control/registerServlet.java
@@ -1,41 +1,36 @@
 package control;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class registerServlet
- */
+import DAO.UserDAOImp;
+import model.User_info;
 @WebServlet("/registerServlet")
 public class registerServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public registerServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("username"));
+        String fn = request.getParameter("fname");
+        String ln = request.getParameter("lname");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password_reg");
+
+        UserDAOImp userDAO = new UserDAOImp();
+        userDAO.insertNewUser(id, fn, ln, email, password);
+
+        User_info user = userDAO.loginUser(email, password);
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            response.sendRedirect("Home.jsp");
+        } else {
+            response.sendRedirect("login.jsp");
+        }
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
