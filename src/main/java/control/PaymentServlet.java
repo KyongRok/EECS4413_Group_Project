3bbcd4599083040b21870a06b24a55bef58a8793
Rosabbagh <140211804+Rosabbagh@users.noreply.github.com>
2023-08-13 00:34:34 -0400

    Add files via upload

---
+++ src/main/java/control/PaymentServlet.java
@@ -0,0 +1,50 @@
+package control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Cart;
+
+@WebServlet("/PaymentServlet")
+public class PaymentServlet extends HttpServlet {
+    private static int paymentCounter = 0; // To keep track of payment requests
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Cart cart = (Cart) session.getAttribute("cart");
+        
+        String cardNumber = request.getParameter("credit_card");
+        String cvvStr = request.getParameter("cvv");
+        int cvv = 0;
+        try {
+            cvv = Integer.parseInt(cvvStr);
+        } catch (NumberFormatException e) {
+            cvv = -1; // Invalid CVV
+        }
+        
+//        paymentCounter++;
+//        boolean paymentApproved = true;
+//        
+//        // Dummy algorithm: Deny every 3rd payment request
+//        if (paymentCounter % 3 == 0) {
+//            paymentApproved = false;
+//        }
+
+
+       // if (paymentApproved) {
+            cart = new Cart();
+            session.setAttribute("cart", cart);
+            request.setAttribute("paymentMessage", "Order Successfully Completed.");
+     //   } else {
+        //    request.setAttribute("paymentMessage", "Credit Card Authorization Failed.");
+      //  }
+  
+        request.getRequestDispatcher("/PaymentResult.jsp").forward(request, response);
+    }
+}
